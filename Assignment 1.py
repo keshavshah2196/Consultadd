3c45971cc63339f2a48e211507cb72bbd1c40af7
keshavshah2196 <64041903+keshavshah2196@users.noreply.github.com>
2020-05-14 16:52:18 -0400

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,77 @@
+#Q1
+print("Q1")
+a,b,c=5,7,"Ktsh"
+print("a=",a)
+print("b=",b)
+print("c=",c)
+print("_-----")
+
+
+#Q2
+print("Q2")
+p=8+9j
+q=67
+print("Elements before swapping",p,q)
+p,q=q,p
+print("Elemens after swappind",p,q)
+print("------")
+
+
+#Q3
+print("Q3")
+d=46
+f=7/5
+print("Original values")
+print("d=",d,"f=",f)
+temp=d
+d=f
+f=temp
+print("Values afetr swapping")
+print("d=",d,"f=",f)
+
+print("------")
+
+
+#Q4
+print("Q4")
+
+
+print("------")
+
+
+
+#Q5
+print("Q5")
+
+b=eval(input("Enter any number"))
+j=eval(input("Enter any value"))
+print("Additin of 2 values=",b+j)
+print("Result number 2:",b+j+30)
+print("------")
+
+
+#Q6
+print("Q6")
+r=eval(input("Enter anu value"))
+k=eval(input("Enetr any number or string"))
+print("Data type for v1st variable is:",type(r))
+print("Data type fpr 2nd variable is:",type(k))
+
+print("------")
+
+
+
+#Q7
+print("Q7")
+
+getValue=5/7
+GetValue=9+8.6j
+GETVALUE="Welcome to training"
+print(getValue,GetValue,GETVALUE)
+print("------")
+
+
+#Q8
+print("Q8")
+print("Yes, the value assigned  to the ‘a’ variable in the second time will be the final value because Python is “Imperative” language such that it changes the state of the variable every time the variable is defined.")
+print("------")
